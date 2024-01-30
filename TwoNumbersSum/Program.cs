a0fa168ca07f8eff558388d0a3a656557dd5ed23
Victoria Parova <ipz224_pvv@student.ztu.edu.ua>
2024-01-30 13:49:03 +0200

    gitignore and net project were added

---
+++ TwoNumbersSum/Program.cs
@@ -0,0 +1,12 @@
+﻿try
+{
+    Console.Write("Enter first number: ");
+    var number1 = decimal.Parse(Console.ReadLine());
+    Console.Write("Enter second number: ");
+    var number2 = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Sum of two numbers ({0};{1}) equals {2}", number1, number2, number1 + number2);
+}
+catch
+{
+    Console.WriteLine("There was occured some error");
+}
